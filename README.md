58eb0d1fbd740b92bb1164120b2c15cb94e01c1c
Smiggle <coreyv@outlook.com>
2019-02-12 11:08:16 -0500

    Making it Blank
    Not for code

--- README.md
+++
@@ -1,3 +0,0 @@
-# Test-Workspace
-My java class workspace
-This is full of Java example programs and stuff and things that I went through in my class at school
--- Test Project/src/firstApplication/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package firstApplication;
-
-public class HelloWorld{
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
