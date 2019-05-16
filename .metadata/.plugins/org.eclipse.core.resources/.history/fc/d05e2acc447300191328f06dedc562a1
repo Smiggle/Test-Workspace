1e1efc04eee9a38e640404b3fc78b59d66fe709a
Smiggle <coreyv@outlook.com>
2019-05-16 08:50:20 -0400

    Added Chapter 7 Exercise 10
    Didnt comment

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/fc/d05e2acc447300191328f06dedc562a1
@@ -0,0 +1,10 @@
+package Chapter_7;
+
+public class Exercise_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Test Project/src/Chapter_7/Exercise_5.java
+++ Test Project/src/Chapter_7/Exercise_5.java
@@ -1,10 +1,41 @@
 package Chapter_7;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class Exercise_5 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double tot = 0;
+		
+		System.out.println("Input your total coinage: ");
+		System.out.println();
+		
+		getDollarAmount(tot);
 
+	}
+	
+	public static void getDollarAmount(double tot) {
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		int quar;
+		int dime;
+		int nick;
+		int pen;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Quarters: ");
+		quar = input.nextInt();
+		System.out.print("Dimes: ");
+		dime = input.nextInt();
+		System.out.print("Nickles: ");
+		nick = input.nextInt();
+		System.out.print("Pennies: ");
+		pen = input.nextInt();
+		
+		tot = (quar * .25) + (dime * .1) + (nick * .05) + (pen * .01);
+		System.out.println("Total: $" +df.format(tot));
 	}
 
 }
